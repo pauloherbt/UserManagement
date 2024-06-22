210f8341470dd209ab81646d01e12ac42291ea5e
peaga <pauloherbert@alu.ufc.br>
2024-06-22 10:46:41 -0300

    fix: migrations

--- migrations/20240621113708_create-user.ts
+++ migrations/20240621113708_create-user.ts
@@ -1,17 +1,17 @@
 import type { Knex } from "knex";
-import { knexConnection } from "../src/db/db";
-
 
 export async function up(knex: Knex): Promise<void> {
-    knexConnection.schema.createTable('users',(table)=>{
+    await knex.schema.createTable('users',(table)=>{
         table.increments('id');
         table.string('name');
         table.bigint('age');
+        table.text('description').notNullable();
+        table.timestamps();
     }).catch((err)=>console.log(err));
 }
 
 
 export async function down(knex: Knex): Promise<void> {
-    knexConnection.schema.dropTable('users');
+    await knex.schema.dropTable('users');
 }
 
