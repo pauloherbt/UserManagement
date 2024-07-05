b201f506bd0c0488daa16ef7ef96c9cf845490db
peaga <pauloherbert@alu.ufc.br>
2024-07-04 21:47:20 -0300

    add: test DB and NODE_ENV

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,14 +1,18 @@
+import { log } from 'console';
 import { config } from 'dotenv'
-import {z} from 'zod'
+import { z } from 'zod'
 
-config();
-
+if (process.env.NODE_ENV === 'test'){
+    config({ path: '.env.test' })
+}
+else
+    config();
 const envSchema = z.object({
     DB_URL: z.string().default(""),
-    PORT: z.string().transform((port)=>parseInt(port)).default('8080')
+    PORT: z.string().transform((port) => parseInt(port)).default('8080')
 })
 
 const envResult = envSchema.safeParse(process.env);
-if(envResult.error)
+if (envResult.error)
     throw new Error(envResult.error.format().toString())
 export const env = envResult.data;
--- src/tests/transactions.spec.ts
+++ src/tests/transactions.spec.ts
@@ -1,16 +1,36 @@
 import request from 'supertest'
-import { describe, expect, it } from "vitest"
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest"
 import { app } from '../server'
 import { Transaction } from '../models/Transaction';
+import { execSync } from "child_process"
 
 describe('unit test transactions routes', () => {
+    
+    beforeEach(()=>{
+        execSync("npx knex migrate:latest")
+    })
+    afterEach(()=>{
+        execSync('npx knex migrate:rollback --all')
+    })
+    
     const url = '/transactions';
-    it('should create a Transaction returning 201', async () => {
+
+    it.skip('should create a Transaction returning 201', async () => {
         const transaction: Transaction = { title: "teste", amount: 100, type: 'credit' }
         const res = await request(app.server)
             .post(url).send(transaction).accept('application/json');
-
+        
         expect(res.status).toEqual(201);
         expect(res.body).contain(transaction)
+    });
+
+    it('should return all transactions by user',async()=>{
+        const transaction: Transaction = { title: "teste", amount: 100, type: 'credit' }
+        const createTransac = await request(app.server).post(url).send(transaction);
+        const cookies = createTransac.get('Set-Cookie') as string[];
+        
+        const rest = await request(app.server).get(url).set('Cookie',cookies);
+        expect(rest.status).equals(200);
+        expect(rest.body.transactions).toEqual([expect.objectContaining(transaction)]);
     })
 })
